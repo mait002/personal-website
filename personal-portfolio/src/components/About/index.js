2c1ab734289a417a8abaff0502d63b2edac1959c
mait002 <maitreyeedas02@gmail.com>
2025-02-22 03:54:27 -0500

    added animation in letters

--- personal-portfolio/src/components/About/index.js
+++ personal-portfolio/src/components/About/index.js
@@ -1,13 +1,26 @@
 
+import { useEffect, useState } from 'react';
 import './index.scss';
+import AnimatedLetters from '../LetterAnimation';
 import DP from '../../assets/images/Mask group.png'
 
 const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const greetingArray = ['H', 'i', ' ', 't', 'h', 'e', 'r', 'e', '!', ' ', 'I', ' ', 'a', 'm', ' ', 'M', 'a', 'i', 't', 'r', 'e', 'y', 'e', 'e', ' ', 'D', 'a', 's', ' ', 'U', 'r', 'm', 'i']
+    useEffect(() => {
+            const timeoutId = setTimeout(() => {
+                setLetterClass('text-animate-hover');
+            }, 4000);
+    
+            return () => clearTimeout(timeoutId); // ✅ Cleanup timeout on unmount
+        }, []);
     return (
         <div className="container about-page">
             <img src={DP} alt='profile picture'/>
             <div className="text-zome">
-                <h1>Hi there, I'm Maitreyee Das Urmi</h1>
+                <h1><AnimatedLetters letterClass={letterClass} 
+                strArray={greetingArray} 
+                idx={0}/></h1>
                 <p>
                 I am a third-year Computer Science student at Toronto Metropolitan University 
                 with a strong passion for web development, machine learning, and database management
--- personal-portfolio/src/components/Home/index.js
+++ personal-portfolio/src/components/Home/index.js
@@ -1,11 +1,26 @@
 
+import { useEffect, useState } from 'react';
 import './index.scss';
+import AnimatedLetters from '../LetterAnimation';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const letterArray = ['M', 'a', 'i', 't', 'r', 'e', 'y', 'e', 'e'];
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+
+        return () => clearTimeout(timeoutId); // ✅ Cleanup timeout on unmount
+    }, []);
+
     return (
         <div className="container home-page">
-            <div className="text-zome">
-                <h1>Meet Maitreyee, a data-driven tech enthusiast with a strong foundation in data science and web development!
+            <div className="text-zone">
+                <h1>Meet <AnimatedLetters letterClass={letterClass} 
+                strArray={letterArray} 
+                idx={15}/>, 
+                a data-driven tech enthusiast with a strong foundation in data science and web development!
                 </h1>
                 
             </div>
